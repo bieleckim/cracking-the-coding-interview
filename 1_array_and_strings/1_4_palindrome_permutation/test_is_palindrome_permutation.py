d0b8f1dde40ad849db9ca7c6238b9066435c958e
Marek Bielecki <marekbielecki93@gmail.com>
2021-11-07 17:28:03 +0100

    fix: add palindrome word to test case name

--- 1_array_and_strings/1_4_palindrome_permutation/test_is_palindrome_permutation.py
+++ 1_array_and_strings/1_4_palindrome_permutation/test_is_palindrome_permutation.py
@@ -4,10 +4,10 @@ from is_palindrome_permutation import is_palindrome_permutation
 
 class TestIsPalindromePermutation(unittest.TestCase):
 
-    def test_should_be_permutation(self) -> None:
+    def test_should_be_palindrome_permutation(self) -> None:
         self.assertTrue(is_palindrome_permutation('a bcab'))
 
-    def test_should_not_be_permutation(self) -> None:
+    def test_should_not_be_palindrome_permutation(self) -> None:
         self.assertFalse(is_palindrome_permutation('ttta aab'))
 
 
